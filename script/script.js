19d72b5084c650b16b86f5a34a8b0dafa78936e9
aja_gorbataja <aja.gorbataja@yandex.ru>
2022-12-01 14:14:10 +0300

    ревью1

--- script/script.js
+++ script/script.js
@@ -4,27 +4,27 @@ let inputName = document.querySelector('.form__input-name');
 let profileName = document.querySelector('.profile__info-name');
 let inputDescription = document.querySelector('.form__input-description');
 let profileDescription = document.querySelector('.profile__info-description');
+let editButton = document.querySelector('.profile__info-edit');
+let closeButton = document.querySelector('.popup__close');
+let form = document.querySelector('.form');
 
 function openPopup() {
   popup.classList.add('popup__open');
   inputName.value = profileName.textContent;
   inputDescription.value = profileDescription.textContent;
 }
-let editButton = document.querySelector('.profile__info-edit');
-editButton.addEventListener('click', openPopup);
 
 function closePopup() {
   popup.classList.remove('popup__open');
 }
-let closeButton = document.querySelector('.popup__close');
-closeButton.addEventListener('click', closePopup);
 
-
-let form = document.querySelector('.form');
 function handleFormSubmit (evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileDescription.textContent = inputDescription.value;
   closePopup()
 }
+
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
 form.addEventListener('submit', handleFormSubmit);
